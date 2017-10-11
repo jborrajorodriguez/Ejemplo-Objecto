1520268a79ac8819df25a454871b45210044fd03
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-10-11 10:02:22 +0200

    Ejemplo

---
+++ Exemplo Obxecto/src/exemplo/obxecto/Semaforo.java
@@ -0,0 +1,13 @@
+
+package exemplo.obxecto;
+public class Semaforo {
+    //atributos
+    private String cor;
+    //metodo acceso.
+    public void darCor(String c){
+        cor= c;
+    }
+    public String dimeCor(){
+        return cor;
+    }
+}
